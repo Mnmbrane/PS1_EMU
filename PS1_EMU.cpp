a727877034f6990db3ce259c8e895af11293c471
Mnmbrane <mnmbrane@gmail.com>
2021-04-13 22:55:11 -0400

    Transfer Source.cpp to PS1_EMU.cpp

--- PS1_EMU.cpp
+++ PS1_EMU.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include <stdio.h>
+#include "CPU.h"
+#include "Memory.h"
 
-void say_hello(){
-    std::cout << "Hello, from PS1_EMU!\n";
-    
+int main()
+{
+   PSEmu::Memory* mem = new PSEmu::Memory();
+   PSEmu::CPU* cpu = new PSEmu::CPU(mem);
+
+   if(cpu != nullptr)
+      delete cpu;
+
+   if(mem != nullptr)
+      delete mem;
+   return 0;
 }
